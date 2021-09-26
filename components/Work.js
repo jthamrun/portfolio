9fa0c86e2dc4bc718bfdd331e53a868dd8b2c42e
Jonathan Thamrun <jthm@Jonathans-Mac-mini.local>
2021-09-25 21:48:29 -0700

    update

--- components/Work.js
+++ components/Work.js
@@ -8,13 +8,13 @@ function Work() {
             </div>
             <div className="mt-16 space-y-5 md:grid md:grid-cols-2 md:grid-flow-row md:gap-y-4 md:gap-x-10 text-center md:mx-6">
                 <div className="col-span-2 bg-white shadow">
-                    <div className="relative pb-1/2 bg-red-500">
+                    <div className="relative pb-1/2 bg-red-500 border border-1">
                         <img
                             className="absolute h-full w-full"
                             src="https://i.imgur.com/AhHctVd.png"
                         />
                     </div>
-                    <div className="py-6 px-4 md:px-8 text-sm text-left font-karla border-t-2">
+                    <div className="py-6 px-4 md:px-8 text-sm text-left font-karla border border-1s">
                         <p className="font-light">
                             <span className="font-semibold">
                                 GREEN - Ongoing Project.
@@ -33,32 +33,38 @@ function Work() {
                 </div>
 
                 <WorkInfo
-                    ImageUrl="https://images.unsplash.com/photo-1551650992-ee4fd47df41f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+                    ImageUrl="https://i.imgur.com/VYpi07O.png"
+                    Title="AIRBNB CLONE - Ongong Project"
                     Text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
                 />
 
                 <WorkInfo
-                    ImageUrl="https://images.unsplash.com/photo-1560461396-41fd0b4f711c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+                    ImageUrl="https://i.imgur.com/oSqoit9.png"
+                    Title="ROOKBOOK"
                     Text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
                 />
 
                 <WorkInfo
-                    ImageUrl="https://images.unsplash.com/photo-1552238979-402eb7a9258c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=990&q=80"
+                    ImageUrl="https://i.imgur.com/ynyDCz3.png"
+                    Title="AMAZON CLONE"
                     Text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
                 />
 
                 <WorkInfo
-                    ImageUrl="https://images.unsplash.com/photo-1558655146-6c222b05fce4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+                    ImageUrl="https://i.imgur.com/sA0BG2n.png"
+                    Title="HULU CLONE"
                     Text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
                 />
 
                 <WorkInfo
-                    ImageUrl="https://images.unsplash.com/photo-1556155092-8707de31f9c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80"
+                    ImageUrl="https://i.imgur.com/zrlaJE5.png"
+                    Title="LINKEDIN CLONE"
                     Text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
                 />
 
                 <WorkInfo
-                    ImageUrl="https://images.unsplash.com/photo-1555631545-d29638363b03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80"
+                    ImageUrl="https://i.imgur.com/rB6apgn.png"
+                    Title="e-Commerce Application"
                     Text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
                 />
             </div>
--- components/WorkInfo.js
+++ components/WorkInfo.js
@@ -1,14 +1,17 @@
-function WorkInfo({ ImageUrl, Text }) {
+function WorkInfo({ ImageUrl, Title, Text }) {
     return (
         <div className="bg-white shadow">
-            <div className="relative pb-9/10 bg-red-500">
+            <div className="relative pb-9/10 bg-red-500 border border-1">
                 <img
                     className="absolute h-full w-full object-cover"
                     src={`${ImageUrl}`}
                 />
             </div>
-            <div className="py-6 px-4 md:px-8 text-sm text-left font-karla">
-                <p>{Text}</p>
+            <div className="py-6 px-4 md:px-8 text-sm text-left font-karla border border-1">
+                <p className="font-light">
+                    <span className="font-semibold">{Title}</span>
+                    &nbsp;&nbsp;&nbsp;{Text}
+                </p>
             </div>
         </div>
     );
