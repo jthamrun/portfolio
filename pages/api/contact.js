9d262f90330e020eb4c5f1969a1f40323a5ee1b4
Jonathan Thamrun <jonathan.thamrun@gmail.com>
2021-09-26 22:01:16 -0700

    Update

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,4 @@
-export default function (req, res) {
+export default async (req, res) => {
     require("dotenv").config();
 
     let nodemailer = require("nodemailer");
@@ -12,6 +12,19 @@ export default function (req, res) {
         secure: true,
     });
 
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+
     const mailData = {
         from: "testjon719@gmail.com",
         to: "jonathan.thamrun@gmail.com",
@@ -20,9 +33,25 @@ export default function (req, res) {
         html: `<div>${req.body.message}</div><p>Sent from:
       ${req.body.email}</p>`,
     };
-    transporter.sendMail(mailData, function (err, info) {
-        if (err) console.log(err);
-        else console.log(info);
+
+    await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
     });
-    res.send("success");
+
+    res.status(200).json({ status: "OK" });
+
+    // transporter.sendMail(mailData, function (err, info) {
+    //     if (err) console.log(err);
+    //     else console.log(info);
+    // });
+    // res.send("success");
 }
